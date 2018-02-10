022bfcb397c4a6b6665a3966cf09c062f393ff61
gmoratorio <moratorio.guillermo@gmail.com>
2018-02-09 17:02:07 -0700

    auth-refactor -- added basic wiring for logout button on MoreScreen

--- src/locale.en.js
+++ src/locale.en.js
@@ -3,11 +3,6 @@ export const login = {
     tagline: "It's time to get out there...",
     login_as_guest: "Continue as guest",
     login_with_facebook: "Log in with Facebook",
-    signup: "Sign up",
-    email: "Email",
-    email_placeholder: "email@gmail.com",
-    password: "Password",
-    password_placeholder: "password",
     errors:{
         auth: "Email and Password combination not recognized",
         general: "There was an error",
@@ -20,4 +15,8 @@ export const campsite = {
     description: "Looking for some secret sites? Want to find a hidden spot? Upload a campsite, and if it's approved, we'll unlock two secret sites that are only available to our contributing members.",
     upload: "Upload Campsite",
 
+};
+
+export const more_screen = {
+  right_nav: 'Log Out'
 };
--- src/screens/MoreScreen.js
+++ src/screens/MoreScreen.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
-import {Icon} from 'react-native-elements';
+import {Platform, Text} from 'react-native';
+import {Icon, Button, Card} from 'react-native-elements';
 
-import {CardSection, Card} from '../components/common/index';
+import {more_screen} from '../locale.en';
+const {right_nav} = more_screen;
 
 class MoreScreen extends Component {
+    static renderRightNavButton = (navigate) => {
+        if (Platform.OS === 'ios') {
+            return (
+                <Button
+                    title={right_nav}
+                    onPress={() => navigate('login')}
+                    backgroundColor="rgba(0,0,0,0)"
+                    color="rgba(0,122,255,1)"
+                />
+            );
+        } else if (Platform.OS === 'android') {
+            // android-specific code for navigation here
+        }
+    };
+
     static navigationOptions = (props) => {
         const {navigation: {navigate}} = props;
 
@@ -12,6 +28,7 @@ class MoreScreen extends Component {
             title: 'More',
             headerTitle: 'More Options',
             headerLeft: null,
+            headerRight: MoreScreen.renderRightNavButton(navigate),
             tabBarIcon: ({focused, tintColor }) => (<Icon type='ionicon' name={focused ? 'ios-more' : 'ios-more-outline'} size={25} color={tintColor} />)
         }
 
@@ -20,11 +37,9 @@ class MoreScreen extends Component {
     render(){
         return(
             <Card>
-                <CardSection>
-                    <Text>
-                        More
-                    </Text>
-                </CardSection>
+                <Text>
+                    More screen
+                </Text>
             </Card>
         );
     }
