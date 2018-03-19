ab46b2d8311acc34b7a885af72380eeb634a9010
Aaron Goodman <amgoodman5@gmail.com>
2018-03-19 17:58:16 -0600

    gh-60 --b component refactor

---
+++ src/components/CampsiteListItem.js
@@ -0,0 +1,107 @@
+// 3rd party libraries - core
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView, ImageBackground} from 'react-native';
+import {Card, ListItem, Button, Tile, List, Image, Badge, Icon} from 'react-native-elements';
+// 3rd party libraries - additional
+// styles and language
+import { badgeGreen, limeGreenTitle, RoyalBlueHighlight, blueGreenNav} from '../styles/index';
+import { campsite } from '../locale.en'
+import _ from 'lodash';
+
+
+const CampsiteListItem = ({site}) => {
+    const{textStyle, IconContainer, subtitleView,siteAvatarStyle, siteAvatarContainerStyle, titleView, nearestTownStyle, badgeWrapperStyle, badgeContainerStyle,  accessibilityStyle} = styles
+    const {campsite_form:{accessibility_options}} = campsite;
+    const {id, title, description, nearestTown, accessibility, siteImageData} = site;
+return(
+    <ListItem
+
+                   avatar={siteImageData ? {uri:`data:image/png;base64,${siteImageData}`} : require('../../assets/starTent.jpg')}
+                  avatarContainerStyle={siteAvatarContainerStyle}
+                  avatarStyle={siteAvatarStyle}
+                  title={
+                      <View>
+                         <Text style={titleView}>{title}</Text>
+                         <Badge
+                           wrapperStyle={badgeWrapperStyle}
+                           containerStyle={badgeContainerStyle}
+                           >
+                             <Text style={nearestTownStyle}>{nearestTown}</Text>
+                         </Badge>
+                         <View style={subtitleView}>
+                             <Text>{description}</Text>
+                         </View>
+                         <View style={subtitleView}>
+
+                               <Text style={accessibilityStyle}>{accessibility_options[accessibility]}</Text>
+                         </View>
+                        </View>
+                        }
+                          subtitle={
+                             <View style={IconContainer}>
+                                <Icon
+                                     name='image-inverted'
+                                     type="entypo"
+                                     color='purple' />
+                                    <Icon
+                                      name='air'
+                                      type="entypo"
+                                      color='#f50' />
+                                    <Icon
+                                      name='drop'
+                                      type="entypo"
+                                      color='#7C9EC2' />
+                                      <Icon
+                                      name='leaf'
+                                      type="entypo"
+                                      color='orange' />
+                                 </View>
+                         }
+                />)
+}
+const styles = StyleSheet.create({
+  IconContainer:{
+    flexDirection:'row',
+    paddingLeft:10,
+    paddingTop: 5
+  },
+  badgeWrapperStyle:{
+    width:'50%',
+     marginLeft:10
+  },
+  badgeContainerStyle:{
+     backgroundColor: blueGreenNav
+  },
+  siteAvatarStyle:{
+    height:'100%',
+    width:'100%'
+  },
+  siteAvatarContainerStyle:{
+     height:'100%',
+     width:'30%'
+   },
+  nearestTownStyle:{
+       color: 'white'
+    },
+  subtitleView: {
+    flexDirection: 'row',
+    paddingLeft: 10,
+    paddingTop: 5
+    },
+  titleView: {
+     fontSize:17,
+     fontWeight: '600',
+     color:limeGreenTitle,
+     flexDirection: 'row',
+     paddingLeft: 10,
+     paddingBottom: 5
+    },
+    accessibilityStyle:{
+      color:'red',
+      borderStyle:'solid'
+    }
+});
+
+
+
+export default CampsiteListItem
--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -2,18 +2,19 @@
 import React from 'react';
 import {View, Text, StyleSheet, ScrollView, ImageBackground} from 'react-native';
 import {Card, ListItem, Button, Tile, List, Image, Badge, Icon} from 'react-native-elements';
+import CampsiteListItem from './CampsiteListItem';
 // 3rd party libraries - additional
 // styles and language
-import { badgeGreen, limeGreenTitle, RoyalBlueHighlight, blueGreenNav} from '../styles/index';
-import { campsite } from '../locale.en'
-const {campsite_form:{accessibility_options}} = campsite;
+
+
+
+
+
 // our components - additional
 import _ from 'lodash';
 
 const SearchList = ({sites, accessibility}) => {
 
-
-  const{textStyle, IconContainer, subtitleView,siteAvatarStyle, siteAvatarContainerStyle, titleView, nearestTownStyle, badgeWrapperStyle, badgeContainerStyle,  accessibilityStyle} = styles
   if(!sites){
     return( <Text> No Sites Available </Text>)
   }else{
@@ -21,54 +22,14 @@ const SearchList = ({sites, accessibility}) => {
   <ScrollView>
      <List>
        {
-         _.map(sites, site => {
-            const {id, title, description, nearestTown, accessibility} = site
-
+         _.map(sites, (site, index) => {
             return (
-                <ListItem
-                  key={id}
-                  avatar={require('../../assets/starTent.jpg')}
-                  avatarContainerStyle={siteAvatarContainerStyle}
-                  avatarStyle={siteAvatarStyle}
-                  title={
-                      <View>
-                         <Text style={titleView}>{title}</Text>
-                         <Badge
-                           wrapperStyle={badgeWrapperStyle}
-                           containerStyle={badgeContainerStyle}
-                           >
-                             <Text style={nearestTownStyle}>{nearestTown}</Text>
-                         </Badge>
-                         <View style={subtitleView}>
-                             <Text>{description}</Text>
-                         </View>
-                         <View style={subtitleView}>
+        <CampsiteListItem
+        key={index}
+        site={site}
 
-                               <Text style={accessibilityStyle}>{accessibility_options[accessibility]}</Text>
-                         </View>
-                        </View>
-                        }
-                          subtitle={
-                             <View style={IconContainer}>
-                                <Icon
-                                     name='image-inverted'
-                                     type="entypo"
-                                     color='purple' />
-                                    <Icon
-                                      name='air'
-                                      type="entypo"
-                                      color='#f50' />
-                                    <Icon
-                                      name='drop'
-                                      type="entypo"
-                                      color='#7C9EC2' />
-                                      <Icon
-                                      name='leaf'
-                                      type="entypo"
-                                      color='orange' />
-                                 </View>
-                         }
-                />
+
+        />
                );
              })
            }
@@ -79,47 +40,5 @@ const SearchList = ({sites, accessibility}) => {
 };
 
 
-    const styles = StyleSheet.create({
-      IconContainer:{
-        flexDirection:'row',
-        paddingLeft:10,
-        paddingTop: 5
-      },
-      badgeWrapperStyle:{
-        width:'50%',
-         marginLeft:10
-      },
-      badgeContainerStyle:{
-         backgroundColor: blueGreenNav
-      },
-      siteAvatarStyle:{
-        height:'100%',
-        width:'100%'
-      },
-      siteAvatarContainerStyle:{
-         minHeight:'100%',
-         width:'30%'
-       },
-      nearestTownStyle:{
-           color: 'white'
-        },
-      subtitleView: {
-        flexDirection: 'row',
-        paddingLeft: 10,
-        paddingTop: 5
-        },
-      titleView: {
-         fontSize:17,
-         fontWeight: '600',
-         color:limeGreenTitle,
-         flexDirection: 'row',
-         paddingLeft: 10,
-         paddingBottom: 5
-        },
-        accessibilityStyle:{
-          color:'red',
-          borderStyle:'solid'
-        }
-    });
 
 export default SearchList;
