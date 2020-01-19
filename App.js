72b0200ed8ba2b1209fc9638f75e652f1f1161ed
gmoratorio <moratorio.guillermo@gmail.com>
2020-01-19 09:51:18 -0700

    GH-130 -- upgraded to Expo SDK 35

--- App.js
+++ App.js
@@ -8,17 +8,13 @@ import {AppLoading} from 'expo';
 
 import {FIREBASE_CONFIG, SENTRY_DSN} from './env';
 
-import Sentry from 'sentry-expo';
-import {SentrySeverity, SentryLog} from 'react-native-sentry';
+import * as Sentry from 'sentry-expo';
 
-Sentry.enableInExpoDevelopment = true;
-
-Sentry.config(
-    SENTRY_DSN,
-    {
-        logLevel: SentryLog.Debug
-    }
-).install();
+Sentry.init({
+    dsn: SENTRY_DSN,
+    enableInExpoDevelopment: true,
+    debug: true
+});
 
 
 import {navKeys} from './src/constants';
